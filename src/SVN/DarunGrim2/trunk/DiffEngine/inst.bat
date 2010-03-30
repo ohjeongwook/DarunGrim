0692cf895c4ae91f3b2dda5a2361999dd47059a6
Matt Oh <oh.jeongwook@gmail.com>
2010-03-30 15:22:48 -0700

    Remove unnecessary files under SVN

--- src/SVN/DarunGrim2/trunk/DiffEngine/inst.bat
+++
@@ -1,5 +0,0 @@
-call "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-nmake 
-dir WIN2000_DEBUG\*.exe
-copy WIN2000_DEBUG\*.exe ..\Bin\
-pause
--- src/SVN/DarunGrim2/trunk/Lib/IDAIPCParam.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-//Shared Memory Name And Size
-#define SHARED_MEMORY_NAME "Shared Memory"
-#define SHARED_MEMORY_SIZE 100000
-//Event Name
-#define READ_EVENT_NAME "ReadEvent"
-#define WRITE_EVENT_NAME "ReadEvent"
