221f6365f39d39290e0566c4d7bac34939afbc1d
Matt Oh <oh.jeongwook@gmail.com>
2010-05-05 09:00:23 -0700

    Test Files

---
+++ src/DiffEngine/Test.cpp
@@ -0,0 +1,30 @@
+#include "IDAClientManager.h"
+#include "Configuration.h"
+#include "DiffMachine.h"
+#include "DataBaseWriter.h"
+#include "XGetopt.h"
+#include "ProcessUtils.h"
+#include "dprintf.h"
+
+#define strtoul10(X) strtoul(X,NULL,10)
+int DebugLevel=0;
+
+void main(int argc,char *argv[])
+{
+	int optind=1;
+
+	int TheSourceFileID=1;
+	int TheTargetFileID=2;
+
+	char *OutputFilename=argv[optind];
+
+	DiffMachine *pDiffMachine;	
+	DBWrapper OutputDB(OutputFilename);
+	CreateTables(OutputDB);
+	pDiffMachine=new DiffMachine();
+	pDiffMachine->Retrieve(OutputDB,TRUE,TheSourceFileID,TheTargetFileID);
+	pDiffMachine->Analyze();
+	pDiffMachine->Save(OutputDB);
+	
+	OutputDB.CloseDatabase();
+}
---
+++ src/DiffEngine/TestAcroForm.bat
@@ -0,0 +1,2 @@
+..\Bin\Test.exe "T:\mat\Projects\Binaries\Adobe\Adobe Systems Incorporated\AcroForm\diff.dgf"
+pause
