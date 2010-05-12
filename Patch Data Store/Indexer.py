7bd86360430d76dbb85e52c90b29227724f5e866
Matt Oh <oh.jeongwook@gmail.com>
2010-05-12 00:42:10 -0700

    Indexer Initial Implementation

---
+++ Patch Data Store/Indexer.py
@@ -0,0 +1,62 @@
+import sqlalchemy
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.orm import mapper, sessionmaker
+
+class Index(object):
+	def __init__( self, operating_system, service_pack, filename, company_name, version_string, patch_identifier, full_path ):
+		self.operating_system = operating_system
+		self.service_pack = service_pack
+		self.filename = filename
+		self.company_name = company_name
+		self.version_string = version_string
+		self.patch_identifier = patch_identifier
+		#TODO: Parser version_string
+		#self.version_number = 
+		#self.release_plan = 
+		self.full_path = full_path
+
+	def __repr__( self ):
+		return "<Index('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s' )>" % ( self.operating_system, self.service_pack, self.filename, self.company_name, self.version_string, self.patch_identifier, self.version_number, self.release_plan, self.full_path )
+
+class Database:
+	DebugLevel = 3
+	def __init__( self, filename ):
+		echo = False
+		if self.DebugLevel > 2:
+			echo = True
+		self.Engine = sqlalchemy.create_engine( 'sqlite:///' + filename, echo = echo )
+		metadata = MetaData()
+
+		self.IndexTable = Table( 'Index', metadata, 
+			Column('id', Integer, primary_key = True ),
+			Column('operating_system', String ),
+			Column('service_pack', String ),
+			Column('filename', String ),
+			Column('company_name', String ),
+			Column('version_string', String ),
+			Column('patch_identifier', String ), #ex) MS09-011
+			Column('version_number', String ),
+			Column('release_plan', String ),
+			Column('full_path', String )
+		)
+		metadata.create_all( self.Engine )
+		mapper( Index, self.IndexTable )
+		
+		Session = sessionmaker()
+		Session.configure( bind = self.Engine )	
+		session = Session()
+
+		operating_system = "os"
+		service_pack = "sp"
+		filename = "fn"
+		company_name = "company"
+		version_string = "version"
+		patch_identifier = "patch"
+		full_path = "full"
+
+		new_record = Index( operating_system, service_pack, filename, company_name, version_string, patch_identifier, full_path )
+		session.add ( new_record )
+		print 'Retrieved:',session.query( Index ).filter_by( operating_system='os').first() 
+
+if __name__ == '__main__':
+	database = Database( 'test.db' )
---
+++ Patch Data Store/TestIndexer.bat
@@ -0,0 +1,2 @@
+c:\python26\python Indexer.py install
+pause
