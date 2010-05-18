0ac1e2730fc671093040e99fe97f30b2519eb2fe
Matt Oh <oh.jeongwook@gmail.com>
2010-05-17 18:52:26 -0700

    Clicker Supporting General Patch Related Dialog Boxes

--- Patch Data Store/Clicker/Clicker.cpp
+++ Patch Data Store/Clicker/Clicker.cpp
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <string>
 #include "Implementation.h"
 
 class WindowDataCollection
 {
 private:
 	int Level;
+	std::string TopLevelText;
+	bool AButtonIsClicked;
+
 public:
 
 	WindowDataCollection(): Level( 0 )
 	{
+		AButtonIsClicked = FALSE;
 	}
 
 	void IncreaseLevel()
@@ -26,6 +31,18 @@ public:
 		return Level;
 	}
 
+	void Click( HWND hwnd )
+	{
+		AButtonIsClicked = TRUE;
+		//SetActiveWindow( hwnd );
+		//SetForegroundWindow( hwnd );
+
+		printf("Sending a Message\n" );
+		::SendMessage( hwnd, BM_SETSTATE, 1, 0 );
+		::Sleep( 1000 );
+		::SendMessage( hwnd, BM_CLICK, 0, 0 );
+	}
+
 	void AddWindowHandle( HWND hwnd )
 	{
 		//Level, handle
@@ -35,20 +52,43 @@ public:
 		}
 		char Buffer[1024];
 		GetWindowText( hwnd, Buffer, sizeof( Buffer ) );
-		//printf("%x: %s\n", hwnd, Buffer );
-		printf("[%s]\n", Buffer );
+
+		DWORD dwProcessId;
+		GetWindowThreadProcessId( hwnd, &dwProcessId );
 
 		if( Level == 1 )
-			SetActiveWindow( hwnd );
-
-		//TODO: Separate clicking thread from enumeration thread
-		if( !strcmp( Buffer, "&Save" ) || !strcmp( Buffer, "&Yes" ) || !strcmp( Buffer, "Yes" )  || !strcmp( Buffer, "OK" )  || !strcmp( Buffer, "&OK" )  || !strcmp( Buffer, "Run" ))
 		{
-			printf("Sending Message to [%s]\n", Buffer );
-			::SendMessage(hwnd, BM_SETSTATE, 1, 0 );
-			printf("Clicked [%s]\n", Buffer );
-			::SendMessage(hwnd, BM_CLICK, 0, 0 );
-			printf("Click Returned [%s]\n", Buffer );
+			TopLevelText = Buffer;
+			AButtonIsClicked  = FALSE;
+		}
+
+		//printf("%x: %s\n", hwnd, Buffer );
+		//printf("[%s] Pid = %d\n", Buffer, dwProcessId );
+		printf("[%s]\n", Buffer );
+
+		if( Level > 1 )
+		{
+			//TODO: Separate clicking thread from enumeration thread
+			/*
+			if( TopLevelText == "Extraction Complete" ||
+				TopLevelText == "Microsoft Internet Explorer Update" ||
+				TopLevelText.find( "Security Update for " ) != std::string::npos ||
+				TopLevelText.find( "Update for" ) != std::string::npos ||
+				TopLevelText.find( "Hotfix" ) != std::string::npos ||
+				TopLevelText.find( "hotfix" ) != std::string::npos ||
+				TopLevelText.find( "Security Patch" ) != std::string::npos
+			)
+			{
+				if( !strcmp( Buffer, "&OK" ) || !strcmp( Buffer, "OK" ) )
+				{
+					Click( hwnd );
+				}
+			}*/
+			if( !strcmp( Buffer, "&OK" ) || !strcmp( Buffer, "OK" ) )
+			{
+				printf("Clicking [%s]\n", TopLevelText.c_str() );
+				Click( hwnd );
+			}
 		}
 	}
 };
@@ -72,9 +112,19 @@ BOOL CALLBACK EnumWindowsProc(
 	return TRUE;
 }
 
-int main()
+void ClickDialogs()
 {
 	//TODO: to do this in time interval
 	WindowDataCollection *pWindowDataCollection = new WindowDataCollection();
-	BOOL ret = EnumWindows( EnumWindowsProc, (LPARAM)  pWindowDataCollection );
+	while( 1 )
+	{
+		BOOL ret = EnumWindows( EnumWindowsProc, (LPARAM)  pWindowDataCollection );
+		//::Sleep(100);
+	}
+}
+
+
+int main()
+{
+	ClickDialogs();
 }
