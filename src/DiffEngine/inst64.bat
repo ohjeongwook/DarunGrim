9dab00749bd24c9d0dca0f1391e894911c1d5d4c
Matt Oh <oh.jeongwook@gmail.com>
2010-05-18 19:40:00 -0700

    Build Files Updated to Support Python Modules

--- src/DiffEngine/inst64.bat
+++ src/DiffEngine/inst64.bat
@@ -2,4 +2,9 @@ call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
 nmake nodebug=1
 dir WIN2000_DEBUG\*.exe
 copy WIN2000_DEBUG\*.exe ..\Bin\
+copy WIN2000_RETAIL\*.exe ..\Bin\
+
+copy WIN2000_RETAIL\*.pyd ..\Bin\
+copy DiffEngine.py ..\Bin\
+
 pause
