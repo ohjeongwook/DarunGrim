3330f53bb2e75bca0d04c280856de168ec44b820
Matt Oh <oh.jeongwook@gmail.com>
2010-05-22 11:16:20 -0700

    Fixed Compilation Warning Messages

--- src/Lib/ProcessUtils.h
+++ src/Lib/ProcessUtils.h
@@ -2,7 +2,7 @@
 
 bool StartProcess(LPTSTR szCmdline);
 void *malloc_wrapper(size_t size);
-void *realloc(void *memblock,size_t old_size,size_t size);
+//void *realloc(void *memblock,size_t old_size,size_t size);
 
 void Execute(bool Wait,const char *format,...);
 char *WriteToTemporaryFile(const char *format,...);
