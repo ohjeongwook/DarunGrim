4c5b37e8d1599e977fcfdb42c79f4425e4189556
Matt Oh <oh.jeongwook@gmail.com>
2010-05-22 18:23:38 -0700

    Build Files Reconfigured

--- src/DiffEngine/inst64.bat
+++ src/DiffEngine/inst64.bat
@@ -1,7 +1,7 @@
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-REM nmake nodebug=1 WIN2000_RETAIL\_DiffEngine.pyd
+nmake nodebug=1 WIN2000_RETAIL\_DiffEngine.pyd
 copy WIN2000_RETAIL\*.pyd ..\..\Bin\
-copy Python\DiffEngine.py ..\..\Bin\
+copy Python\*.py ..\..\Bin\
 
 nmake WIN2000_DEBUG\DarunGrim2C.exe 
 copy WIN2000_DEBUG\*.exe ..\..\Bin\
