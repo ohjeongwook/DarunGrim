f48e5baa4214be74a0325e74e5011bddb24a7392
Matt Oh <oh.jeongwook@gmail.com>
2010-05-22 21:43:39 -0700

    Updated Build Files

--- src/DiffEngine/inst64.bat
+++ src/DiffEngine/inst64.bat
@@ -1,9 +1,11 @@
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-nmake nodebug=1 WIN2000_RETAIL\_DiffEngine.pyd
+nmake nodebug=1
 copy WIN2000_RETAIL\*.pyd ..\..\Bin\
+copy WIN2000_RETAIL\*.exe ..\..\Bin\
 copy Python\*.py ..\..\Bin\
 
 nmake WIN2000_DEBUG\DarunGrim2C.exe 
-copy WIN2000_DEBUG\*.exe ..\..\Bin\
+copy WIN2000_DEBUG\DarunGrim2C.exe  ..\..\Bin\DarunGrim2CDebug.exe 
+copy WIN2000_DEBUG\DarunGrim2C.pdb  ..\..\Bin\DarunGrim2CDebug.pdb
 
 pause
