3a4196790fbde9e1ffa699ca2602f5162376624c
Matt Oh <oh.jeongwook@gmail.com>
2010-05-29 01:06:19 -0700

    Added GetFunctionDisasmLinesMap, GetMatchMapForFunction, GetDisasmComparisonTable, GetDisasmComparisonTableByFunctionAddress methods

--- Diff Inspector/DarunGrimDatabaseWrapper.py
+++ Diff Inspector/DarunGrimDatabaseWrapper.py
@@ -147,6 +147,146 @@ class Database:
 	def GetFunctionMatchInfo( self ):
 		return self.SessionInstance.query( FunctionMatchInfo ).all()		
 
+	def GetFunctionDisasmLinesMap( self, file_id, function_address):
+		disasm_lines_hash={}
+
+		for one_location_info in self.SessionInstance.query( OneLocationInfo ).filter_by( file_id = file_id, function_address=function_address ).all():
+			disasm_lines_hash[one_location_info.start_address] = one_location_info.disasm_lines
+		return disasm_lines_hash
+
+	def GetMatchMapForFunction( self, file_id, function_address ):
+		match_hash = {}
+		for match_map in self.SessionInstance.query( MatchMap ).filter( MatchMap.source_address.in_(self.SessionInstance.query( OneLocationInfo.start_address ).filter_by( file_id = file_id, function_address=function_address))).all():
+			match_hash[ match_map.source_address ] = match_map.target_address
+		return match_hash
+
+	def GetDisasmComparisonTable( self, source_data, target_data, map ):
+		DebugLevel = 0
+		left_addresses = source_data.keys()
+		left_addresses.sort()
+	
+		right_addresses = target_data.keys()
+		right_addresses.sort()
+		
+		right_addressesIndex={}	
+		index = 0
+		for address in right_addresses:
+			right_addressesIndex[address] = index
+			index += 1
+		
+		DisasmTable={}
+		Checkedright_addresses={}
+		left_address_index = 0
+		for left_address in left_addresses:
+			right_address_index = None
+			maximum_index = left_address_index
+			if map.has_key( left_address ):
+				right_address = map[ left_address ]
+				Checkedright_addresses[ right_address ] = 1
+				
+				if right_addressesIndex.has_key( right_address ):
+					right_address_index = right_addressesIndex[right_address]
+					if DebugLevel > 2:
+						print left_address_index, right_address_index
+					
+					if left_address_index > right_address_index:
+						maximum_index = left_address_index
+					elif left_address_index < right_address_index:
+						maximum_index = right_address_index
+	
+			while DisasmTable.has_key( maximum_index ):
+				maximum_index += 1
+	
+			DisasmTable[ maximum_index ] = ( left_address_index, right_address_index )
+			left_address_index += 1
+	
+		for right_address in right_addressesIndex:
+			if not Checkedright_addresses.has_key( right_address ):
+				right_address_index = right_addressesIndex[right_address]
+				
+				NewDisasmTable = {}
+				if DisasmTable.has_key( right_address_index ):
+					for index in DisasmTable.keys():
+						if index >= right_address_index:
+							NewDisasmTable[ index + 1 ] = DisasmTable[ index ]
+						else:
+							NewDisasmTable[ index ] = DisasmTable[ index ]
+					NewDisasmTable[right_address_index] = ( None, right_address_index )
+					DisasmTable = NewDisasmTable
+				else:
+					DisasmTable[right_address_index] = ( None, right_address_index )
+	
+		if DebugLevel > 2:
+			print DisasmTable
+		indexes = DisasmTable.keys()
+		indexes.sort()
+	
+		disasm_lines=[]
+	
+		for index in indexes:
+			( left_address_index, right_address_index ) = DisasmTable[index]
+	
+			left_address = 0
+			right_address = 0
+			if left_address_index:
+				left_address = left_addresses[ left_address_index ]
+				
+			if right_address_index:
+				right_address = right_addresses[ right_address_index ]
+	
+			disasm_lines.append( ( hex(left_address)+":", hex(right_address)+":" ) )
+			if DebugLevel > 2:
+				print index, ':', left_address_index, hex(left_address), right_address_index, hex(right_address)
+	
+			left_lines=()
+			right_lines=()
+	
+			if source_data.has_key(left_address):
+				left_lines = source_data[ left_address ].split('\n')
+	
+			if target_data.has_key(right_address):
+				right_lines = target_data[ right_address ].split('\n')
+		
+			if DebugLevel > 2:
+				print 'Split Lines'
+				print left_lines, right_lines
+	
+			i = 0
+			while 1:
+				left_line = ''
+				if len(left_lines) > i:
+					left_line = left_lines[i]
+				right_line = ''
+				if len(right_lines) > i:
+					right_line = right_lines[i]
+	
+				if left_line=='' and right_line=='':
+					break
+	
+				disasm_lines.append( (left_line, right_line) )
+				i += 1
+			disasm_lines.append( ( "", "" ) )
+	
+		return disasm_lines
+
+	def GetMatchedFunctionDiffLines( self, source_function_address, target_function_address ):
+		source_disasm_lines_hash = self.GetFunctionDisasmLinesMap( 1, source_function_address )
+		target_disasm_lines_hash = self.GetFunctionDisasmLinesMap( 2, target_function_address )
+		match_map = self.GetMatchMapForFunction( 1, source_function_address )
+		disasm_lines = self.GetDisasmComparisonTable( source_disasm_lines_hash, target_disasm_lines_hash, match_map )
+
+		disasm_comparion_table = ''
+		maximum_left_line_length = 0
+		for (left_line, right_line) in disasm_lines:
+			if len( left_line ) > maximum_left_line_length:
+				maximum_left_line_length = len( left_line )
+		
+		maximum_left_line_length += 10
+		for (left_line, right_line) in disasm_lines:
+			space_len = maximum_left_line_length - len( left_line )
+			disasm_comparion_table += left_line + " " * space_len + right_line + '\n'
+
+		return disasm_comparion_table
 
 if __name__ == '__main__':
 	import sys
@@ -163,4 +303,7 @@ if __name__ == '__main__':
 			print str(function_match_info.type) + '\t',
 			print str( function_match_info.match_rate ) + "%" + '\t',
 			print function_match_info.match_count_for_the_source, function_match_info.non_match_count_for_the_source, function_match_info.match_count_with_modificationfor_the_source, function_match_info.match_count_for_the_target, function_match_info.non_match_count_for_the_target, function_match_info.match_count_with_modification_for_the_target
+			#print database.GetFunctionDisasmLinesMap( function_match_info.source_file_id, function_match_info.source_address )
+			#print database.GetMatchMapForFunction( function_match_info.source_file_id, function_match_info.source_address )
+			print database.GetMatchedFunctionDiffLines( function_match_info.source_address, function_match_info.target_address )
 
