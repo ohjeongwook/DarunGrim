e573e58cb1973252f70046be7e6b08b91047e8bd
unknown <moh@.websense.com>
2010-06-21 11:44:36 -0700

    Updated install script

--- src/DiffEngine/inst.bat
+++ src/DiffEngine/inst.bat
@@ -1,5 +1,11 @@
-call "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-nmake 
-dir WIN2000_DEBUG\*.exe
-copy WIN2000_DEBUG\*.exe ..\..\Bin\
+call "%ProgramFiles%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
+nmake nodebug=1
+copy WIN2000_RETAIL\*.pyd ..\..\Bin\
+copy WIN2000_RETAIL\*.exe ..\..\Bin\
+copy Python\*.py ..\..\Bin\
+
+nmake WIN2000_DEBUG\DarunGrim2C.exe 
+copy WIN2000_DEBUG\DarunGrim2C.exe  ..\..\Bin\DarunGrim2CDebug.exe 
+copy WIN2000_DEBUG\DarunGrim2C.pdb  ..\..\Bin\DarunGrim2CDebug.pdb
+
 pause
