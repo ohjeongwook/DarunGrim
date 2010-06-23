81bbf58b0101e46f20382ed456320359ecaeca17
Matt Oh <oh.jeongwook@gmail.com>
2010-06-22 18:14:37 -0700

    Persistent HTMLGenerator object

--- UI/Web/WebServer.py
+++ UI/Web/WebServer.py
@@ -9,47 +9,39 @@ import os
 
 class WebServer(object):
 	def __init__(self):
-		pass
+		self.HTMLWorker = HTMLGenerator.Worker()
 
 	def index(self):
-		worker = HTMLGenerator.Worker()
-		return worker.Index()
+		return self.HTMLWorker.Index()
 	index.exposed = True
 
 	def MSPatchList(self, operation = '' ):
 		print 'MSPatchList'
-		worker = HTMLGenerator.Worker()
-		return worker.MSPatchList( operation )
+		return self.HTMLWorker.MSPatchList( operation )
 	MSPatchList.exposed = True
 
 	def PatchInfo(self,id):
-		worker = HTMLGenerator.Worker()
-		return worker.PatchInfo( id )
+		return self.HTMLWorker.PatchInfo( id )
 	PatchInfo.exposed = True
 
 	def DownloadInfo(self, patch_id, id, operation = '' ):
-		worker = HTMLGenerator.Worker()
-		return worker.DownloadInfo( patch_id, id, operation )
+		return self.HTMLWorker.DownloadInfo( patch_id, id, operation )
 	DownloadInfo.exposed = True
 
 	def FileInfo(self, patch_id, download_id, id):
-		worker = HTMLGenerator.Worker()
-		return worker.FileInfo( patch_id, download_id, id )
+		return self.HTMLWorker.FileInfo( patch_id, download_id, id )
 	FileInfo.exposed = True
 
 	def StartDiff( self, patch_id, download_id, file_id, source_id, target_id, show_detail = 0 ):
-		worker = HTMLGenerator.Worker()
-		return worker.StartDiff( patch_id, download_id, file_id, source_id, target_id, show_detail )
+		return self.HTMLWorker.StartDiff( patch_id, download_id, file_id, source_id, target_id, show_detail )
 	StartDiff.exposed = True
 
 	def ShowFunctionMatchInfo( self, patch_id, download_id, file_id, source_id, target_id ):
-		worker = HTMLGenerator.Worker()
-		return worker.GetFunctionMatchInfo( patch_id, download_id, file_id, source_id, target_id )
+		return self.HTMLWorker.GetFunctionMatchInfo( patch_id, download_id, file_id, source_id, target_id )
 	ShowFunctionMatchInfo.exposed = True
 
 	def ShowBasicBlockMatchInfo( self, patch_id, download_id, file_id, source_id, target_id, source_address, target_address ):
-		worker = HTMLGenerator.Worker()
-		return worker.GetDisasmComparisonTextByFunctionAddress( patch_id, download_id, file_id, source_id, target_id, source_address, target_address )
+		return self.HTMLWorker.GetDisasmComparisonTextByFunctionAddress( patch_id, download_id, file_id, source_id, target_id, source_address, target_address )
 	ShowBasicBlockMatchInfo.exposed = True
 
 if __name__ == '__main__':
