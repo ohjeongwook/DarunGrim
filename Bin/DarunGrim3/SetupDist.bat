01d0dbe9af1bbfaaab66e0bbf91ea682ec33ac02
Matt Oh <oh.jeongwook@gmail.com>
2010-07-22 00:03:42 -0700

    Updated installation scripts

--- Bin/DarunGrim3/SetupDist.bat
+++ Bin/DarunGrim3/SetupDist.bat
@@ -1,3 +1,10 @@
 c:\python26\python SetupDist.py py2exe
+move bin\WebServer.exe bin\DarunGrim3Web.exe
 copy DarunGrim3.cfg bin
+mkdir ..\DarunGrim3.Bin
+move bin\* ..\DarunGrim3.Bin
+mkdir ..\DarunGrim3.Bin\Plugin
+copy ..\Plugin\* ..\DarunGrim3.Bin\Plugin
+copy ..\DarunGrim2\* ..\DarunGrim3.Bin
+xcopy /S /I data ..\DarunGrim3.Bin\data
 pause
