c69c31e501b8e43a34c6f084176a52f225f51c51
Jeongwook Oh <oh.jeongwook@gmail.com>
2010-08-13 18:09:05 -0700

    Fixed compilation issues

--- Src/DarunGrim2/Plugin/inst.bat
+++ Src/DarunGrim2/Plugin/inst.bat
@@ -1,7 +1,7 @@
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
 nmake
-mkdir ..\..\Bin\Plugin
-copy bin\*.plw ..\..\Bin\Plugin
+mkdir ..\..\..\Bin\Plugin
+copy bin\*.plw ..\..\..\Bin\Plugin
 copy bin\*.plw "%ProgramFiles%\IDA\plugins"
 pause
 
