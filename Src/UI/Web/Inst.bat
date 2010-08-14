a050e20ab31b8391e06c131eaded40d579822c34
Jeongwook Oh <oh.jeongwook@gmail.com>
2010-08-13 18:35:14 -0700

    Corrected Bin directory

--- Src/UI/Web/Inst.bat
+++ Src/UI/Web/Inst.bat
@@ -1,12 +1,12 @@
 mkdir ..\..\Bin\DarunGrim3
 copy *.py ..\..\Bin\DarunGrim3
 copy *.bat ..\..\Bin\DarunGrim3
-copy "DarunGrim3Sample01.cfg"  ..\..\Bin\DarunGrim3\DarunGrim3.cfg
-xcopy /s /I data ..\..\Bin\DarunGrim3\data
-copy "..\..\Bin Collector\*.py" ..\..\Bin\DarunGrim3
-copy "..\..\Diff Inspector\*.py" ..\..\Bin\DarunGrim3
-copy "..\..\Bin Collector\Bin\*.pyd" ..\..\Bin\DarunGrim3
-xcopy /s /I ..\..\Bin\DarunGrim3 ..\..\Bin\DarunGrim3.Test
-pushd ..\..\Bin\DarunGrim3
+copy "DarunGrim3Sample01.cfg"  ..\..\..\Bin\DarunGrim3\DarunGrim3.cfg
+xcopy /s /I data ..\..\..\Bin\DarunGrim3\data
+copy "..\..\Bin Collector\*.py" ..\..\..\Bin\DarunGrim3
+copy "..\..\Diff Inspector\*.py" ..\..\..\Bin\DarunGrim3
+copy "..\..\Bin Collector\Bin\*.pyd" ..\..\..\Bin\DarunGrim3
+xcopy /s /I ..\..\Bin\DarunGrim3 ..\..\..\Bin\DarunGrim3.Test
+pushd ..\..\..\Bin\DarunGrim3
 SetupDist.bat
 pause
