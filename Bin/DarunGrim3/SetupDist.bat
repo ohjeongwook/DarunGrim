2f7325ec72fb509cec3c8c9faf7609bb6a9f0eb0
Jeongwook Oh <oh.jeongwook@gmail.com>
2010-08-23 16:03:48 -0700

    Consolidated install script for DarunGrim3

--- Bin/DarunGrim3/SetupDist.bat
+++ Bin/DarunGrim3/SetupDist.bat
@@ -1,10 +1,42 @@
-c:\python26\python SetupDist.py py2exe
+REM Clean Up
+rmdir /Q /S Src
+rmdir /Q /S Bin
+del /Q DarunGrim3.zip
 
-copy DarunGrim3.cfg bin
-mkdir ..\..\Publish\DarunGrim3\Bin
-move bin\* ..\..\Publish\DarunGrim3\Bin
-mkdir ..\..\Publish\DarunGrim3\Bin\Plugin
-copy ..\Plugin\* ..\..\Publish\DarunGrim3\Bin\Plugin
-copy ..\DarunGrim2\* ..\..\Publish\DarunGrim3\Bin
-xcopy /S /I data ..\..\Publish\DarunGrim3\Bin\Data
+REM Start copying files
+mkdir Src
+copy ..\..\Src\UI\Web\*.py Src\
+copy ..\..\Src\UI\Web\*.bat Src\
+xcopy /y /s /I ..\..\Src\UI\Web\data Src\data
+copy "..\..\Src\Bin Collector\*.py" Src\
+copy "..\..\Src\Diff Inspector\*.py" Src\
+copy "..\..\Src\Bin Collector\Bin\*.pyd" Src\
+copy SetupDist.py Src
+copy ..\..\Src\UI\Web\DarunGrim3Sample01.cfg Src\DarunGrim3.cfg
+
+REM Generate binaries
+pushd Src
+c:\python26\python SetupDist.py py2exe
+popd
+
+REM Copy necessary files
+copy ..\..\Src\UI\Web\DarunGrim3Sample01.cfg Src\bin\DarunGrim3.cfg
+copy ..\DarunGrim2\* Src\bin
+copy ..\..\Publish\Docs\*.pdf Src\bin
+
+REM Clean up some unncessary files
+del /Q Src\Bin\w9xpopen.exe
+del /Q Src\Bin\Test.exe
+del /Q Src\Bin\tcl*.dll
+del /Q Src\Bin\tk*.dll
+rmdir /Q /S Src\Bin\tcl
+
+REM Put data directory to binary directory
+xcopy /y /s /I ..\..\Src\UI\Web\data Src\bin\data
+
+REM move bin directory location
+mv Src\bin Bin
+
+REM zip a package
+zip DarunGrim3.zip Bin\*
 pause
