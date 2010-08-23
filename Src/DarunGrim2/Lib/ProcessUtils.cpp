05c56d15a40811c59f5e894b3b2eba0e3b2c05d3
Jeongwook Oh <oh.jeongwook@gmail.com>
2010-08-23 15:17:37 -0700

    Rewrote WriteToTemporaryFile to use *.idc file instead of *.tmp file

--- Src/DarunGrim2/Lib/ProcessUtils.cpp
+++ Src/DarunGrim2/Lib/ProcessUtils.cpp
@@ -81,67 +81,59 @@ void *realloc(void *memblock,size_t old_size,size_t size)
 char *WriteToTemporaryFile(const char *format,...)
 {
 #define BUFSIZE 512
-	HANDLE hTempFile;
-	char szTempName[BUFSIZE];  
-	char lpPathBuffer[BUFSIZE];
-	DWORD dwBufSize=BUFSIZE;
-	DWORD dwRetVal;
-	DWORD uRetVal;
+	HANDLE temporary_file_handle;
+	char temporary_filename[BUFSIZE];  
+	char temporary_path[BUFSIZE];
+	DWORD buffer_size=BUFSIZE;
 
 	// Get the temp path.
-	dwRetVal=GetTempPath(dwBufSize,	// length of the buffer
-			lpPathBuffer); // buffer for path 
-	if(dwRetVal > dwBufSize ||(dwRetVal==0))
+	DWORD return_value=GetTempPath(buffer_size,	// length of the buffer
+			temporary_path); // buffer for path 
+	if(return_value > buffer_size ||(return_value==0))
 	{
 		printf("GetTempPath failed with error %d.\n",GetLastError());
 		return NULL;
 	}
 
-	// Create a temporary file. 
-	uRetVal=GetTempFileName(lpPathBuffer,// directory for tmp files
-			"DarunGrim",		// temp file name prefix 
-			0,			// create unique name 
-			szTempName); // buffer for name 
-	if(uRetVal==0)
+	temporary_filename[ sizeof(temporary_filename) - sizeof( char ) ] = NULL;
+	if( _snprintf( temporary_filename, sizeof(temporary_filename) - sizeof( char ),
+			"%s\\DarunGrim-%x-%x.idc", temporary_path, GetCurrentProcessId(), GetCurrentThreadId() ) > 0 )
 	{
-		printf("GetTempFileName failed with error %d.\n",
-				GetLastError());
-		return NULL;
+		// Create the new file to write the upper-case version to.
+		temporary_file_handle=CreateFile((LPTSTR)temporary_filename,// file name 
+				GENERIC_READ | GENERIC_WRITE,// open r-w 
+				0,					// do not share 
+				NULL,				// default security 
+				CREATE_ALWAYS,		// overwrite existing
+				FILE_ATTRIBUTE_NORMAL,// normal file 
+				NULL);				// no template 
+		if(temporary_file_handle==INVALID_HANDLE_VALUE) 
+		{ 
+			printf("CreateFile failed with error %d.\n",GetLastError());
+			return NULL;
+		} 
+
+		va_list args;
+		va_start(args,format);
+		char Contents[1024]={0,};
+		_vsnprintf(Contents,sizeof(Contents)/sizeof(char),format,args);
+		va_end(args);
+
+		DWORD dwBytesWritten;
+		BOOL fSuccess=WriteFile(temporary_file_handle,
+			Contents,
+			strlen(Contents),
+			&dwBytesWritten,
+			NULL); 
+		if(!fSuccess) 
+		{
+			printf("WriteFile failed with error %d.\n",GetLastError());
+			return NULL;
+		}
+		CloseHandle(temporary_file_handle);
+		return _strdup(temporary_filename);
 	}
-
-	// Create the new file to write the upper-case version to.
-	hTempFile=CreateFile((LPTSTR)szTempName,// file name 
-			GENERIC_READ | GENERIC_WRITE,// open r-w 
-			0,					// do not share 
-			NULL,				// default security 
-			CREATE_ALWAYS,		// overwrite existing
-			FILE_ATTRIBUTE_NORMAL,// normal file 
-			NULL);				// no template 
-	if(hTempFile==INVALID_HANDLE_VALUE) 
-	{ 
-		printf("CreateFile failed with error %d.\n",GetLastError());
-		return NULL;
-	} 
-
-	va_list args;
-	va_start(args,format);
-	char Contents[1024]={0,};
-	_vsnprintf(Contents,sizeof(Contents)/sizeof(char),format,args);
-	va_end(args);
-
-	DWORD dwBytesWritten;
-	BOOL fSuccess=WriteFile(hTempFile,
-		Contents,
-		strlen(Contents),
-		&dwBytesWritten,
-		NULL); 
-	if(!fSuccess) 
-	{
-		printf("WriteFile failed with error %d.\n",GetLastError());
-		return NULL;
-	}
-	CloseHandle(hTempFile);
-	return _strdup(szTempName);
+	return NULL;
 }
 
 void Execute(bool Wait,const char *format,...)
