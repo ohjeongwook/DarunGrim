c13fbdb09feb9b0da1bf9d7473e4a42785d74e33
Jeongwook Oh <oh.jeongwook@gmail.com>
2010-08-23 15:32:35 -0700

    Consolidate install scripts in one file for multiple platforms

--- Src/Bin Collector/Clicker/inst64.bat
+++
@@ -1,5 +0,0 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-nmake
-dir WIN2000_DEBUG\*.exe
-copy WIN2000_DEBUG\*.exe ..\Bin\
-pause
--- Src/DarunGrim2/Plugin/inst64.bat
+++
@@ -1,7 +0,0 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-REM C:\Program Files (x86)\IDA\plugins
-nmake
-mkdir ..\..\..\Bin\Plugin
-copy bin\*.plw ..\..\..\Bin\Plugin
-copy bin\*.plw "%ProgramFiles(x86)%\IDA\plugins"
-pause
