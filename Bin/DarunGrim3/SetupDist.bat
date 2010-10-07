400adf327d371dce316e8b677e18c14be5d98d2d
Jeongwook Oh <oh.jeongwook@gmail.com>
2010-10-06 23:16:07 -0700

    Separated src copying batch script from SetupDist.bat

--- Bin/DarunGrim3/SetupDist.bat
+++ Bin/DarunGrim3/SetupDist.bat
@@ -3,26 +3,21 @@ rmdir /Q /S Src
 rmdir /Q /S Bin
 del /Q DarunGrim3.zip
 
-REM Start copying files
-mkdir Src
-copy ..\..\Src\UI\Web\*.py Src\
-copy ..\..\Src\UI\Web\*.bat Src\
-xcopy /y /s /I ..\..\Src\UI\Web\data Src\data
-copy "..\..\Src\Bin Collector\*.py" Src\
-copy "..\..\Src\Diff Inspector\*.py" Src\
-copy "..\..\Src\Bin Collector\Bin\*.pyd" Src\
-copy SetupDist.py Src
-copy ..\..\Src\UI\Web\DarunGrim3Sample01.cfg Src\DarunGrim3.cfg
+mkdir Src\bin
+copy ..\DarunGrim2\* Src\bin\
+copy ..\..\Publish\Docs\*.pdf Src\bin
+
+call CopySrc.bat
 
 REM Generate binaries
 pushd Src
 c:\python26\python SetupDist.py py2exe
 popd
 
+REM Prepare binary directory
 REM Copy necessary files
 copy ..\..\Src\UI\Web\DarunGrim3Sample01.cfg Src\bin\DarunGrim3.cfg
-copy ..\DarunGrim2\* Src\bin
-copy ..\..\Publish\Docs\*.pdf Src\bin
+
 
 REM Clean up some unncessary files
 del /Q Src\Bin\w9xpopen.exe
