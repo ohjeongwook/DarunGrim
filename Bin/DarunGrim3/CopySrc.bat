0165e7fab7afae38dddfdc0430d912c91027b9b3
Jeongwook Oh <oh.jeongwook@gmail.com>
2010-10-07 15:38:41 -0700

    Use xcopy to copy source files

--- Bin/DarunGrim3/CopySrc.bat
+++ Bin/DarunGrim3/CopySrc.bat
@@ -1,10 +1,11 @@
 REM Start copying files
 mkdir Src
-copy ..\..\Src\UI\Web\*.py Src\
-copy ..\..\Src\UI\Web\*.bat Src\
-xcopy /y /s /I ..\..\Src\UI\Web\data Src\data
-copy "..\..\Src\Bin Collector\*.py" Src\
-copy "..\..\Src\Diff Inspector\*.py" Src\
-copy "..\..\Src\Bin Collector\Bin\*.pyd" Src\
-copy SetupDist.py Src\
-copy ..\..\Src\UI\Web\DarunGrim3Sample01.cfg Src\DarunGrim3.cfg
+
+xcopy /D /S /I /Y ..\..\Src\UI\Web\*.py Src\
+xcopy /D /S /I /Y ..\..\Src\UI\Web\*.bat Src\
+xcopy /D /S /I /Y ..\..\Src\UI\Web\data Src\data
+xcopy /D /S /I /Y "..\..\Src\Bin Collector\*.py" Src\
+xcopy /D /S /I /Y "..\..\Src\Diff Inspector\*.py" Src\
+xcopy /D /S /I /Y "..\..\Src\Bin Collector\Bin\*.pyd" Src\
+xcopy /D /S /I /Y SetupDist.py Src\
+xcopy /D /S /I /Y ..\..\Src\UI\Web\DarunGrim3Sample01.cfg Src\DarunGrim3.cfg
