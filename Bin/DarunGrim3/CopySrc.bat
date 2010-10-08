dab0ce93f2ba6708e05f10e209b09915404bc89f
Jeongwook Oh <oh.jeongwook@gmail.com>
2010-10-08 16:42:38 -0700

    Use copy for configuration file copy

--- Bin/DarunGrim3/CopySrc.bat
+++ Bin/DarunGrim3/CopySrc.bat
@@ -8,4 +8,4 @@ xcopy /D /S /I /Y "..\..\Src\Bin Collector\*.py" Src\
 xcopy /D /S /I /Y "..\..\Src\Diff Inspector\*.py" Src\
 xcopy /D /S /I /Y "..\..\Src\Bin Collector\Bin\*.pyd" Src\
 xcopy /D /S /I /Y SetupDist.py Src\
-xcopy /D /S /I /Y ..\..\Src\UI\Web\DarunGrim3Sample01.cfg Src\DarunGrim3.cfg
+copy ..\..\Src\UI\Web\DarunGrim3Sample01.cfg Src\DarunGrim3.cfg
