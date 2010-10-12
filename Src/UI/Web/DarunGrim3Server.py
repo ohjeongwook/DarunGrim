bebf394656c23d67af8a07f60181f1155056a3b5
Jeongwook Oh <oh.jeongwook@gmail.com>
2010-10-11 17:21:47 -0700

    Made debugging message more readable.

--- Src/UI/Web/DarunGrim3Server.py
+++ Src/UI/Web/DarunGrim3Server.py
@@ -690,7 +690,7 @@ if __name__ == '__main__':
 	if len( sys.argv ) > 1:
 		config_file = sys.argv[1]
 
-	print 'Configuration file is' + config_file
+	print 'Configuration file is ' + config_file
 	fd = open( config_file )
 	config_data = fd.read()
 	fd.close()
