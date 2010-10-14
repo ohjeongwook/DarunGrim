74eadc2e18c3bca521eebae38e0218cf2f840475
Jeongwook Oh <oh.jeongwook@gmail.com>
2010-10-14 16:48:35 -0700

    Setting DARUNGRIM2_ROOT

--- Src/DarunGrim2/DiffEngine/inst.bat
+++ Src/DarunGrim2/DiffEngine/inst.bat
@@ -1,5 +1,6 @@
 IF EXIST "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat" call "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
 IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat" call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
+set DARUNGRIM2_ROOT=..
 nmake nodebug=1
 copy WIN2000_RETAIL\*.pyd ..\..\..\Bin\DarunGrim2
 copy WIN2000_RETAIL\*.exe ..\..\..\Bin\DarunGrim2
--- Src/DarunGrim2/Plugin/inst.bat
+++ Src/DarunGrim2/Plugin/inst.bat
@@ -1,5 +1,6 @@
 IF EXIST "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat" call "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
 IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat" call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
+set DARUNGRIM2_ROOT=..
 nmake
 mkdir ..\..\..\Bin\Plugin
 copy bin\*.plw ..\..\..\Bin\Plugin
