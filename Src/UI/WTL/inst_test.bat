415fb23be51828dca917a726c9ff3cb0f06d0532
Jeong Wook Oh <oh.jeongwook@gmail.com>
2014-04-12 01:03:53 -0700

    Added WTL missing files Removed unused files Renamed Python to Scripts Renamed DarunGrimEngine.py to DarunGrim.py

--- Src/UI/WTL/inst_test.bat
+++
@@ -1,9 +0,0 @@
-IF EXIST "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat" call "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat" call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-nmake
-rem > err.txt
-rem notepad err.txt
-dir WIN2000_DEBUG\*.exe
-copy WIN2000_DEBUG\*.exe ..\..\Bin\
-copy WIN2000_DEBUG\*.exe "C:\Program Files\DarunGrim2"
-pause
