0fd50386b3e2ff5b1fd2896b05151dc93012dbdf
ohjeongwook <oh.jeongwook@gmail.com>
2014-11-10 14:24:45 -0800

    First version of PySide GUI with Graph drawing class

---
+++ Src/Scripts/DarunGrim.py
@@ -0,0 +1,257 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+from PySide.QtSql import *
+import DarunGrimDatabase
+from Graphs import *
+import pprint
+import FlowGrapher
+
+class FunctionMatchTable(QAbstractTableModel):
+	Debug=0
+	def __init__(self,parent, database_name, *args):
+		QAbstractTableModel.__init__(self,parent,*args)
+		self.database = DarunGrimDatabase.Database(database_name)
+
+		self.match_list=[]
+		self.full_fmi_list=[]
+		for function_match_info in self.database.GetFunctionMatchInfo():
+			if function_match_info.non_match_count_for_the_source > 0 or function_match_info.non_match_count_for_the_target > 0:
+				#print function_match_info.id, function_match_info.source_file_id, function_match_info.target_file_id, function_match_info.end_address, 
+			
+				if self.Debug>0:
+					print "%s\t%s\t%s\t%s\t%s%%\t%d\t%d\t%d\t%d\t%d\t%d" % (function_match_info.source_function_name,
+															function_match_info.target_function_name,
+															str(function_match_info.block_type),
+															str(function_match_info.type),
+															str( function_match_info.match_rate ),
+															function_match_info.match_count_for_the_source, 
+															function_match_info.non_match_count_for_the_source, 
+															function_match_info.match_count_with_modificationfor_the_source, 
+															function_match_info.match_count_for_the_target, 
+															function_match_info.non_match_count_for_the_target, 
+															function_match_info.match_count_with_modification_for_the_target)
+
+				self.match_list.append([function_match_info.source_function_name,
+									function_match_info.target_function_name,
+									str( function_match_info.match_rate)])
+
+				self.full_fmi_list.append(function_match_info)
+
+	def GetFunctionAddresses(self,index):
+		return [self.full_fmi_list[index].source_address, self.full_fmi_list[index].target_address]
+
+	def rowCount(self,parent):
+		return len(self.match_list)
+	
+	def columnCount(self,parent):
+		return 3
+
+	def data(self,index,role):
+		if not index.isValid():
+			return None
+		elif role!=Qt.DisplayRole:
+			return None
+
+		return self.match_list[index.row()][index.column()]
+
+	def headerData(self,col,orientation,role):
+		if orientation==Qt.Horizontal and role==Qt.DisplayRole:
+			return ["Orig", "Patched", "Match"][col]
+		return None
+
+	def sort(self,col,order):
+		pass
+
+class BlockMatchTable(QAbstractTableModel):
+	def __init__(self,parent, *args):
+		QAbstractTableModel.__init__(self,parent,*args)
+
+		self.match_list=[]
+
+	def ShowFunctionAddresses(self,match_list):
+		self.match_list=match_list
+		self.dataChanged.emit(0, len(self.match_list))
+
+	def rowCount(self,parent):
+		return len(self.match_list)
+	
+	def columnCount(self,parent):
+		return 3
+
+	def data(self,index,role):
+		if not index.isValid():
+			return None
+		elif role!=Qt.DisplayRole:
+			return None
+
+		return self.match_list[index.row()][index.column()]
+
+	def headerData(self,col,orientation,role):
+		if orientation==Qt.Horizontal and role==Qt.DisplayRole:
+			return ["Orig", "Patched", "Match"][col]
+		return None
+
+	def sort(self,col,order):
+		pass
+
+class MainWindow(QMainWindow):
+	def __init__(self,database_name):
+		super(MainWindow,self).__init__()
+		self.DatabaseName=database_name
+		self.setWindowTitle("DarunGrim 4")
+
+		# Functions
+		self.functions_match_table_model=FunctionMatchTable(self,self.DatabaseName)
+		view=QTableView()
+		view.setModel(self.functions_match_table_model)
+		view.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+		vheader=QHeaderView(Qt.Orientation.Vertical)
+		vheader.setResizeMode(QHeaderView.ResizeToContents)
+		view.setVerticalHeader(vheader)
+
+		hheader=QHeaderView(Qt.Orientation.Horizontal)
+		hheader.setResizeMode(QHeaderView.Stretch)
+		view.setHorizontalHeader(hheader)
+
+		selection=view.selectionModel()
+		selection.selectionChanged.connect(self.handleFunctionMatchTableChanged)
+
+		dock=QDockWidget("Functions",self)
+		dock.setAllowedAreas(Qt.LeftDockWidgetArea|Qt.RightDockWidgetArea)
+		dock.setWidget(view)
+		self.addDockWidget(Qt.BottomDockWidgetArea,dock)
+		self.functions_match_table_view=view
+		self.functions_match_table_dock=dock
+
+		# Blocks
+		self.block_table_model=BlockMatchTable(self)
+		view=QTableView()
+		view.setModel(self.block_table_model)
+		view.setSelectionBehavior(QAbstractItemView.SelectRows)
+		
+		vheader=QHeaderView(Qt.Orientation.Vertical)
+		vheader.setResizeMode(QHeaderView.ResizeToContents)
+		view.setVerticalHeader(vheader)
+
+		hheader=QHeaderView(Qt.Orientation.Horizontal)
+		hheader.setResizeMode(QHeaderView.Stretch)
+		view.setHorizontalHeader(hheader)
+
+		dock=QDockWidget("Blocks",self)
+		dock.setAllowedAreas(Qt.LeftDockWidgetArea|Qt.RightDockWidgetArea)
+		dock.setWidget(view)
+		self.addDockWidget(Qt.BottomDockWidgetArea,dock)
+		self.block_table_view=view
+
+		# Function Graph
+		self.OrigFunctionGraph=FunctionGraphScene()
+		view=QGraphicsView(self.OrigFunctionGraph)
+		view.setRenderHints(QPainter.Antialiasing)
+
+		dock=QDockWidget("Orig",self)
+		dock.setAllowedAreas(Qt.LeftDockWidgetArea|Qt.RightDockWidgetArea)
+		dock.setWidget(view)
+		self.addDockWidget(Qt.TopDockWidgetArea,dock)
+
+		# Function Graph
+		self.PatchedFunctionGraph=FunctionGraphScene()
+		view=QGraphicsView(self.PatchedFunctionGraph)
+		view.setRenderHints(QPainter.Antialiasing)
+
+		dock=QDockWidget("Patched",self)
+		dock.setAllowedAreas(Qt.LeftDockWidgetArea|Qt.RightDockWidgetArea)
+		dock.setWidget(view)
+		self.addDockWidget(Qt.TopDockWidgetArea,dock)
+
+		self.readSettings()
+
+	def DrawFunctionGraph(self,type,function_address,graph_scene,match_info):
+		database=DarunGrimDatabase.Database(self.DatabaseName)
+
+		(source_disasms, source_links) = database.GetFunctionDisasmLines(type, function_address)
+		flow_grapher=FlowGrapher.FlowGrapher()
+		
+		for (address,disasm) in source_disasms.items():
+			if not match_info.has_key(address):
+				flow_grapher.SetNodeShape("white", "red", "Verdana", "12")
+			else:
+				if match_info[address][1]!=100:
+					flow_grapher.SetNodeShape("black", "yellow", "Verdana", "12")
+				else:
+					flow_grapher.SetNodeShape("black", "white", "Verdana", "12")
+
+			name="%.8X" % address
+			flow_grapher.AddNode(address, name, str(disasm))
+
+		for (src,dsts) in source_links.items():
+			for dst in dsts:
+				flow_grapher.AddLink(src,dst)
+		graph_scene.Draw(flow_grapher)
+
+	def handleFunctionMatchTableChanged(self,selected,dselected):
+		for item in selected:
+			for index in item.indexes():
+				[source_function_address, target_function_address] = self.functions_match_table_model.GetFunctionAddresses(index.row())
+				database = DarunGrimDatabase.Database(self.DatabaseName)
+
+				match_list=[]
+				source_match_info={}
+				target_match_info={}
+				for ( source_address, ( target_address, match_rate ) ) in database.GetBlockMatches( source_function_address, target_function_address ):
+					match_list.append(["%x" % source_address, "%x" % target_address, "%d%%" % match_rate])
+					source_match_info[source_address]=[target_address, match_rate]
+					target_match_info[target_address]=[source_address, match_rate]
+
+				self.block_table_model=BlockMatchTable(self)
+				self.block_table_model.ShowFunctionAddresses(match_list)
+				self.block_table_view.setModel(self.block_table_model)
+
+				# Draw graphs
+				self.DrawFunctionGraph("Source", source_function_address, self.OrigFunctionGraph, source_match_info)
+				self.DrawFunctionGraph("Target", target_function_address, self.PatchedFunctionGraph, target_match_info)
+
+				break
+
+	def readSettings(self):
+		settings=QSettings("DarunGrim LLC", "DarunGrim")
+
+		if settings.contains("geometry"):
+			self.restoreGeometry(settings.value("geometry"))
+		else:
+			self.resize(800,600)
+
+		if settings.contains("geometry/functions_match_table"):
+			self.functions_match_table_dock.restoreGeometry(settings.value("geometry/functions_match_table_dock"))
+		else:
+			self.functions_match_table_dock.resize(200,400)
+
+		"""
+		if settings.contains("geometry/block_table_view"):
+			self.block_table_view.restoreGeometry(settings.value("geometry/block_table_view"))
+		else:
+			self.block_table_view.resize(200,400)
+		"""
+
+		self.restoreState(settings.value("windowState"))
+
+	def closeEvent(self, event):
+		settings = QSettings("DarunGrim LLC", "DarunGrim")
+		settings.setValue("geometry", self.saveGeometry())
+		settings.setValue("geometry/functions_match_table_dock", self.functions_match_table_dock.saveGeometry())
+		#settings.setValue("geometry/block_table_view", self.block_table_view.saveGeometry())
+		settings.setValue("windowState", self.saveState())
+		QMainWindow.closeEvent(self, event)
+
+	def resizeEvent(self,event):
+		print 'resizeEvent'
+		#self.functions_match_table_dock.resize(200,400)
+
+if __name__=='__main__':
+	import sys
+
+	database_name=sys.argv[1]
+	app=QApplication(sys.argv)
+	mainWindow=MainWindow(database_name)
+	mainWindow.show()
+	sys.exit(app.exec_())
--- Src/Scripts/Graphs.py
+++ Src/Scripts/Graphs.py
@@ -12,33 +12,31 @@ TYPE_DI_FILLCOLOR=4
 TYPE_DI_BGCOLOR=5
 TYPE_DI_FONTCOLOR=6
 
-class GraphScene(QGraphicsScene):
+class FunctionGraphScene(QGraphicsScene):
 	Debug=0
 	def __init__(self,parent=None):
 		QGraphicsScene.__init__(self,parent)
 
-		flow_grapher=FlowGrapher.FlowGrapher()
-		flow_grapher.SetNodeShape("green", "blue", "10")
-		flow_grapher.AddNode(0, "Test 0", "Disasm lines")
-		flow_grapher.AddNode(1, "Test 1", "Disasm lines")
-		flow_grapher.AddNode(2, "Test 2", "Disasm lines")
-		flow_grapher.AddNode(3, "Test 3", "Disasm lines")
-		flow_grapher.AddLink(0,1)
-		flow_grapher.AddLink(0,2)
-		flow_grapher.AddLink(0,3)
-		flow_grapher.AddLink(1,3)
-		flow_grapher.AddLink(1,4)
+	def InvertedQPointF(self,x,y):
+		return QPointF(x,self.GraphRect[3]-y)
 
+	def Draw(self,flow_grapher):
+		self.clear()
+		self.setSceneRect(-180,-90,360,180)
 		flow_grapher.GenerateDrawingInfo()
 		len=flow_grapher.GetDrawingInfoLength()
 
 		pen_color=''
 		font_size=''
 		font_name=''
+
+		self.GraphRect=[0,0,0,0]
+
 		for i in range(0,len,1):
 			di=flow_grapher.GetDrawingInfoMember(i)
 
 			if di.type==TYPE_DI_GRAPH:
+				self.GraphRect=[di.GetPoint(0).x, di.GetPoint(0).y,di.GetPoint(1).x, di.GetPoint(1).y]
 				self.setSceneRect(QRectF(di.GetPoint(0).x, di.GetPoint(0).y,di.GetPoint(1).x, di.GetPoint(1).y))
 
 			if di.type==TYPE_DI_COLOR:
@@ -54,14 +52,9 @@ class GraphScene(QGraphicsScene):
 				font_color=di.text
 
 			if di.type==TYPE_DI_RECTS:
-					print 'pen_color: %s' % pen_color
-					print 'fill_color: %s' % fill_color
-					print 'bg_color: %s' % bg_color
-					print 'font_color: %s' % font_color
-
 					polygon=QPolygonF()
 					for j in range(0, di.count,1):
-						polygon.append(QPointF(di.GetPoint(j).x, di.GetPoint(j).y))
+						polygon.append(self.InvertedQPointF(di.GetPoint(j).x, di.GetPoint(j).y))
 
 					pen=QPen(QColor(pen_color))
 					brush=QBrush(QColor(bg_color))
@@ -79,14 +72,14 @@ class GraphScene(QGraphicsScene):
 					if self.Debug>0:
 						print 'Line %d,%d - %d,%d' % (di.GetPoint(0).x, di.GetPoint(0).y, di.GetPoint(1).x, di.GetPoint(1).y)
 					
-					line=QGraphicsLineItem(QLineF(QPointF(start_x,start_y),QPointF(end_x,end_y)))
+					line=QGraphicsLineItem(QLineF(self.InvertedQPointF(start_x,start_y),self.InvertedQPointF(end_x,end_y)))
 					line.setPen(QPen(Qt.black))
 					self.addItem(line)
 
 				elif type_ch=='P' or type_ch=='p':
 					polygon=QPolygonF()
 					for j in range(0, di.count,1):
-						polygon.append(QPointF(di.GetPoint(j).x, di.GetPoint(j).y))
+						polygon.append(self.InvertedQPointF(di.GetPoint(j).x, di.GetPoint(j).y))
 
 					pen=None
 					brush=None
@@ -100,18 +93,17 @@ class GraphScene(QGraphicsScene):
 					self.addPolygon(polygon, pen, brush)
 
 				elif type_ch=='B' or type_ch=='b':
-
 					print 'Bezier:'
 					for j in range(0, di.count,1):
 						print '\t%d,%d' % (di.GetPoint(j).x, di.GetPoint(j).y)
 
 					for i in range(0,di.count-1,3):
-						path=QPainterPath(QPointF(di.GetPoint(i).x, di.GetPoint(i).y))
+						path=QPainterPath(self.InvertedQPointF(di.GetPoint(i).x, di.GetPoint(i).y))
 					
 						path.cubicTo(
-										QPointF(di.GetPoint(i+1).x, di.GetPoint(i+1).y),
-										QPointF(di.GetPoint(i+2).x, di.GetPoint(i+2).y),
-										QPointF(di.GetPoint(i+3).x, di.GetPoint(i+3).y)
+										self.InvertedQPointF(di.GetPoint(i+1).x, di.GetPoint(i+1).y),
+										self.InvertedQPointF(di.GetPoint(i+2).x, di.GetPoint(i+2).y),
+										self.InvertedQPointF(di.GetPoint(i+3).x, di.GetPoint(i+3).y)
 									)
 						self.addPath(path);
 
@@ -139,10 +131,9 @@ class GraphScene(QGraphicsScene):
 					text_item.setFont(font)
 					text_item.setPlainText(di.text)
 					w=text_item.boundingRect().width()
-					text_item.setPos(di.GetPoint(0).x-w/2, di.GetPoint(0).y-font_size/2)
+					text_item.setPos(di.GetPoint(0).x-w/2, (self.GraphRect[3] - di.GetPoint(0).y) - font_size - 3 )
 
 					self.addItem(text_item)
-
 
 	def GetColor(self, color_str):
 		if color_str:
@@ -161,24 +152,37 @@ class GraphScene(QGraphicsScene):
 		color.setAlpha(alpha)
 		return color
 
-class MainWindow(QMainWindow):
-	def __init__(self):
-		QMainWindow.__init__(self)
-		scene=GraphScene()
-		self.scene=scene
-
-		layout=QHBoxLayout()
-		self.view=QGraphicsView(self.scene)
-		self.view.setRenderHints(QPainter.Antialiasing)
-		layout.addWidget(self.view)
-
-		self.widget=QWidget()
-		self.widget.setLayout(layout)
-
-		self.setCentralWidget(self.widget)
-		self.setWindowTitle("Graph")
-
 if __name__=='__main__':
+	class MainWindow(QMainWindow):
+		def __init__(self):
+			QMainWindow.__init__(self)
+			scene=FunctionGraphScene()
+			self.scene=scene
+
+			flow_grapher=FlowGrapher.FlowGrapher()
+			flow_grapher.SetNodeShape("black", "white", "Verdana", "10")
+			flow_grapher.AddNode(0, "Test 0", "Disasm lines")
+			flow_grapher.AddNode(1, "Test 1", "Disasm lines")
+			flow_grapher.AddNode(2, "Test 2", "Disasm lines")
+			flow_grapher.AddNode(3, "Test 3", "Disasm lines")
+			flow_grapher.AddLink(0,1)
+			flow_grapher.AddLink(0,2)
+			flow_grapher.AddLink(0,3)
+			flow_grapher.AddLink(1,3)
+			flow_grapher.AddLink(1,4)
+			scene.Draw(flow_grapher)
+
+			layout=QHBoxLayout()
+			self.view=QGraphicsView(self.scene)
+			self.view.setRenderHints(QPainter.Antialiasing)
+			layout.addWidget(self.view)
+
+			self.widget=QWidget()
+			self.widget.setLayout(layout)
+
+			self.setCentralWidget(self.widget)
+			self.setWindowTitle("Graph")
+
 	app=QApplication(sys.argv)
 	frame=MainWindow()
 	frame.setGeometry(100,100,800,500)
