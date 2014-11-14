1ff345f342611dd288cd5df63a98a589a6502d4b
ohjeongwook <oh.jeongwook@gmail.com>
2014-11-13 16:57:54 -0800

    DarunGrimPlugin test scripts

---
+++ Src/Scripts/Test/RunDarunGrimIDC.cmd
@@ -0,0 +1,5 @@
+copy /y C:\mat\Src\DarunGrim\Debug\DarunGrimPlugin.plw "C:\Program Files (x86)\IDA 6.6\plugins"
+
+"C:\Program Files (x86)\IDA 6.6\idaq.exe" -S"%CD%\RunDarunGrim.idc" "Z:\DarunGrimStore\7b\35\40\0d\schannel.dll" 
+
+start Z:\DarunGrimStore\7b\35\40\0d
