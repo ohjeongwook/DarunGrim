151ecc8dcd8d8b56715a933e0f99ed9fb9d43045
ohjeongwook <oh.jeongwook@gmail.com>
2014-11-29 14:59:35 -0800

    Updated install scripts with more dependencies included

--- Src/Scripts/DarunGrim(PyInstaller.Spec).cmd
+++ Src/Scripts/DarunGrim(PyInstaller.Spec).cmd
@@ -1,6 +1,13 @@
 set PYTHONPATH=%PYTHONPATH%;C:\mat\Src\DarunGrim\Src\UI\FlowGrapher;C:\mat\Src\DarunGrim\Src\Scripts\FileManagement
 pyinstaller DarunGrim.spec
 copy /Y ..\..\bin\GraphViz\* dist\DarunGrim
-copy /Y dist\DarunGrim\* ..\..\Release
+
 copy ..\..\Release\DarunGrimPlugin.plw dist\DarunGrim
+copy ..\..\Release\DarunGrimC.exe dist\DarunGrim
+
+mkdir dist\DarunGrim\DarunGrim3
+xcopy ..\..\Release\* dist\DarunGrim\DarunGrim3
+
+mkdir dist\DarunGrim\x64
+xcopy ..\..\x64\Release-x64\* dist\DarunGrim\x64
 pause
