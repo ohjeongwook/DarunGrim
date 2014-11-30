3c0fac7c9a4f6e35b557a6f776631cb103ba8dce
ohjeongwook <oh.jeongwook@gmail.com>
2014-11-30 10:50:55 -0800

    Updated setup.cmd for win32ver module

--- Src/Scripts/FileManagement/setup.cmd
+++ Src/Scripts/FileManagement/setup.cmd
@@ -1,4 +1,5 @@
-SET VS90COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\ 
-c:\python27\python install
+SET VS90COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools"
+SET PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC;%PATH%
+c:\python27\python setup.py install
 pause
 
