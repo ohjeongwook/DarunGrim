13e372750bcb2b2b833e442890f609a379085b92
ohjeongwook <oh.jeongwook@gmail.com>
2014-12-07 23:17:13 -0800

    Updated ParseBulletinHtml to parse more cases

--- Src/Scripts/FileManagement/MSPatchWeb.py
+++ Src/Scripts/FileManagement/MSPatchWeb.py
@@ -80,8 +80,9 @@ class PatchDownloader:
 					print ''
 		return family_id
 
-	def DownloadMSPatch( self, year, bulletin_number, download_patch_files = False ):
-		url = 'https://technet.microsoft.com/en-us/library/security/ms%.2d-%.3d.aspx' % ( year, bulletin_number )
+	def DownloadMSPatch( self, year, bulletin_number, filename, download_patch_files = False ):
+		bulletin_name='ms%.2d-%.3d' % ( year-2000, bulletin_number )
+		url = 'https://technet.microsoft.com/en-us/library/security/%s.aspx'  % bulletin_name
 
 		print 'Opening %s' % url
 		try:
@@ -89,22 +90,21 @@ class PatchDownloader:
 		except:
 			if self.ShowErrorMessage:
 				print 'Downloading Failed'
-			return None
+			return False
 
 		print 'Downloaded %d bytes' % len(data)
-		cache_filename='MS%.2d-%.3d.html' % ( year, bulletin_number )
-		fd=open(cache_filename,"w")
+		fd=open(filename,"w")
 		fd.write(data)
 		fd.close()
 
-		return self.ParseBulletin(data, url, 'MS%.2d-%.3d' % ( year, bulletin_number ))
+		return True
 
 	def ParseTable(self,start_tag):
 		# Go through each tr and td inside them
 		table_info=[]
 		tr_members = []
-		if table_tag:
-			for tr in table_tag.findAll( "tr" ):
+		if start_tag:
+			for tr in start_tag.findAll( "tr" ):
 				td_members = []
 				for td in tr.findAll( "td" ):
 					td_member = {}
@@ -119,13 +119,14 @@ class PatchDownloader:
 							td_member['text'] += tag.string
 					
 					td_members.append( td_member )
-				tr_members.append( td_members )
+
+				if len(td_members)>0:
+					tr_members.append( td_members )
 
 		for td_members in tr_members[1:]:
 			column = 0 
 			td_member_hash = {}
 			for td_member in td_members:
-
 				td_member_hash[ tr_members[0][column]['text'] ] = td_member
 				column += 1
 
@@ -142,14 +143,22 @@ class PatchDownloader:
 				elif family_id:
 					td_member['files'] = ( family_id )
 			"""
-					
-			if self.DebugLevel > -3:
-				pprint.pprint(table_info)
+
+		if self.DebugLevel > 3:
+			print ''
+			print '='*80
+			pprint.pprint(table_info)
 
 		return table_info
 
-	def ParseBulletin(self,data, url='', label='', download_patch_files=False):
-		soup = BeautifulSoup( data )
+	def ParseBulletinHtml(self, filename, url='', label='', download_patch_files=False):
+		fd=open(filename,'r')
+		data=fd.read()
+		fd.close()
+
+		print 'Parsing %d bytes' % len(data)
+
+		soup = BeautifulSoup(data)
 
 		title = ''
 		for h1_tag in soup.find( "h1" ):
@@ -182,13 +191,15 @@ class PatchDownloader:
 
 		for h3_tag in soup.findAll( "h3" ):
 			if h3_tag.string == 'Vulnerability Details':
-				print 'h3_tag',h3_tag.string
+				if self.DebugLevel > 3:
+					print 'h3_tag',h3_tag.string
 				pass
 
 		for table_tag in soup.findAll( "table" ):
 			for tr in table_tag.findAll( "tr" ):
 				for td in tr.findAll( "td" ):
-					print 'td: [%s]' % td.getText()
+					if self.DebugLevel > 3:
+						print 'td: [%s]' % td.getText()
 					break
 				break
 
@@ -196,29 +207,132 @@ class PatchDownloader:
 		patch_info['HtmlData'] = data
 
 		affected_software_table_infos=[]
-		for h3_tag in soup.findAll( "h3" ):
-			if h3_tag.text == 'Affected Software' or h3_tag.text == 'Affected Software:' or h3_tag.text == 'Affected Components:':
-				# Find affected software table
-				current_tag=h3_tag.nextSibling
+
+		found_affected_software_tag=False
+		for h2_tag in soup.findAll("h2"):
+			text=h2_tag.text.strip()
+			if text=='Affected and Non-Affected Software':
+				found_affected_software_tag=True
+				current_tag=h2_tag.nextSibling
 				while current_tag:
-					try:
+					if current_tag.name=='div':
+						for child in current_tag.children:
+							if child.name=='table':
+								affected_software_table_infos.append(self.ParseTable(child))
+					current_tag=current_tag.nextSibling
+
+			if found_affected_software_tag:
+				break
+
+		if not found_affected_software_tag:
+			for h4_tag in soup.findAll("h4"):
+				text=h4_tag.text.strip()
+				if text=='Affected and Non-Affected Software':
+					found_affected_software_tag=True
+					current_tag=h4_tag.nextSibling
+					while current_tag:
 						if current_tag.name=='table':
 							affected_software_table_infos.append(self.ParseTable(current_tag))
-					except:
-						pass
-					current_tag = current_tag.nextSibling
+						elif current_tag.name=='div':
+							break
+						current_tag=current_tag.nextSibling
 
+				if found_affected_software_tag:
+					break
+
+		if not found_affected_software_tag:
+			for h3_tag in soup.findAll("h3"):
+				if h3_tag.text == 'Affected Software' or h3_tag.text == 'Affected Software:' or h3_tag.text == 'Affected Components:':
+					found_affected_software_tag=True
+					# Find affected software table
+					current_tag=h3_tag.nextSibling
+					while current_tag:
+						try:
+							if current_tag.name=='table':
+								affected_software_table_infos.append(self.ParseTable(current_tag))
+						except:
+							import traceback
+							traceback.print_exc()
+
+						current_tag = current_tag.nextSibling
+
+				if found_affected_software_tag:
+					break
+
+		if not found_affected_software_tag:
+			for p_tag in soup.findAll("p"):
+				p_tag_text=p_tag.text.strip()
+				if p_tag_text == 'Affected Software' or \
+					p_tag_text == 'Affected Software:' or \
+					p_tag_text == 'Affected Components:':
+					found_affected_software_tag=True
+					# Find affected software table
+					found_table=False
+					current_tag=p_tag.nextSibling
+					while current_tag:
+						try:
+							if current_tag.name=='table':
+								found_table=True
+								affected_software_table_infos.append(self.ParseTable(current_tag))
+						except:
+							import traceback
+							traceback.print_exc()
+
+						current_tag = current_tag.nextSibling
+
+					if not found_table:
+						current_tag=p_tag.nextSibling
+						while current_tag:
+							try:
+								if current_tag.name=='ul':
+									for child in current_tag.children:
+										if child.name=='li':
+											os=self.RemoveSpecialChar(child.text)
+											print ' '+os
+											for child2 in child.children:
+												if child2.name == 'a':
+													print ' '*2+child2.attrs['href']
+												elif child2.name == 'ul':
+													for child3 in child2.children:
+														print ' '*3+child3.text
+														for child4 in child3.children:
+															if child4.name=='a':
+																print ' '*4+child4.attrs['href']
+								elif current_tag.name=='p':
+									break
+							except:
+								import traceback
+								traceback.print_exc()
+
+							current_tag = current_tag.nextSibling
+
+				if found_affected_software_tag:
+					break
+
+		for affected_software_table_info in affected_software_table_infos:
+			print '-'*80
+			pprint.pprint(affected_software_table_info)
 		return ( patch_info, affected_software_table_infos )
 
-	def DownloadMSPatchAndIndex( self, year, bulletin_number, download_patch_files = False ):
-		name = 'MS%.2d-%.3d' % ( year, bulletin_number )
+	def RemoveSpecialChar(self,text):
+		ret=''
+		for ch in text:
+			if ord(ch)<127:
+				ret+=ch
+			elif ord(ch)==8211:
+				ret+='-'
+
+		return ret
+	def DownloadMSPatchAndIndex( self, year, bulletin_number, bulletin_stroage, download_patch_files = False ):
+		name = 'MS%.2d-%.3d' % ( year-2000, bulletin_number )
 
 		if self.Database!=None:
 			if self.Database.GetPatch( name ):
 				return ( {},{} )
 
 		print 'Downloading',name
-		ret = self.DownloadMSPatch( year, bulletin_number, download_patch_files )
+		filename=os.path.join(bulletin_stroage, '%s.html' % name)
+		ret = self.DownloadMSPatch( year, bulletin_number, filename, download_patch_files )
 
 		if not ret:
 			if self.ShowErrorMessage:
@@ -313,14 +427,13 @@ class PatchDownloader:
 
 	def DownloadCurrentYearPatches( self ):
 		now = datetime.datetime.now()
-		year = now.year - 2000
+		year = now.year
 		return self.DownloadPatches( year, 1, year, 999 )
 
 if __name__ == '__main__':
 	patch_downloader = PatchDownloader( "Patches" )
-	#patch_downloader.DownloadCurrentYearPatches()
-
-	fd=open('MS14-001.html','r')
-	data=fd.read()
-	fd.close()
-	patch_downloader.ParseBulletin(data)
+	year=2005
+	bulletin_number=2
+	filename='test1.html'
+	if patch_downloader.DownloadMSPatch( year, bulletin_number, filename, False ):
+		patch_downloader.ParseBulletinHtml(filename)
