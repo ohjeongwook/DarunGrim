dce00b7c7dba9ff86e58914dbd307f4e778aa7b5
Matt Oh <oh.jeongwook@gmail.com>
2020-05-23 12:53:20 -0700

    Moved out DisassemblyStorage and IDASession code to IDAHasher

--- Src/DGEngine/Utility.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-unsigned char HexToChar(char *Hex);
-unsigned char *HexToBytes(char *HexBytes, int *pLen);
-unsigned char *HexToBytesWithLengthAmble(char *HexBytes);
-char *BytesWithLengthAmbleToHex(unsigned char *Bytes);
-int IsEqualByteWithLengthAmble(unsigned char *Bytes01, unsigned char *Bytes02);
